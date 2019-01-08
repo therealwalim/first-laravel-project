56934124105221a0e934ab4cc60f38077a110fee
Walim <machine-hb@hotmail.fr>
2019-01-08 22:10:37 +0100

    Laravel Project
    First laravel project

---
+++ blog/resources/views/welcome.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Walimify | Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+                
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+                background-color: #21D4FD;
+                background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
+
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 70px;
+                color: white;
+                margin-bottom: 140px;
+            }
+
+            ul {
+                margin-right: 100px;
+            }
+
+            li {
+                display: inline-block;
+                text-decoration: none;
+                margin-left: 55px;
+                margin-bottom: 20px;
+                font-family: 'Lato Regular';
+                font-size: 25px;
+                padding-bottom: 5px;
+                color: white;
+                margin-top: 100px;
+            }
+            li:hover {
+                opacity: 0.6;
+                cursor: pointer;
+            }
+            svg {
+                height: 60px;
+                margin-bottom: 150px;
+                margin-top: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<g>
+	<path d="M395.748,272.046c-0.646-64.841,52.88-95.938,55.271-97.483c-30.075-44.01-76.925-50.039-93.62-50.736   c-39.871-4.037-77.798,23.474-98.033,23.474c-20.184,0-51.409-22.877-84.476-22.276c-43.458,0.646-83.529,25.269-105.906,64.19   c-45.152,78.35-11.563,194.42,32.445,257.963c21.504,31.104,47.146,66.038,80.813,64.79c32.421-1.294,44.681-20.979,83.878-20.979   c39.196,0,50.215,20.979,84.524,20.335c34.888-0.648,56.991-31.699,78.347-62.898c24.694-36.084,34.862-71.019,35.462-72.812   C463.678,375.26,396.422,349.495,395.748,272.046z M331.28,81.761C349.149,60.082,361.21,30.005,357.92,0   c-25.739,1.048-56.938,17.145-75.405,38.775c-16.57,19.188-31.075,49.813-27.188,79.218   C284.061,120.235,313.392,103.391,331.28,81.761z" fill="#FFFFFF"/>
+</g>
+</svg>
+                <div class="title">Walimify</div>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
